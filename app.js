d040bda07daf415c9b18625416a113951f6a6af4
Sylvester <sylvester@erbsledet.dk>
2025-10-29 13:58:11 +0100

    test

--- app.js
+++ app.js
@@ -60,22 +60,22 @@ async function hubspotGetFromEndpoint(type, id) {
 app.post("/hubspot", async (req, res) => {
     const event = req.body;
     console.log(event)
-    if (event.subscriptionType === "object.creation") {
+    if (event[0].subscriptionType === "object.creation") {
         console.log("Det er creation")
-        if (event.objectTypeId === "0-3") {
+        if (event[0].objectTypeId === "0-3") {
             console.log("Det er deal")
-            const deal = await hubspotGetFromEndpoint(event.objectTypeId, event.objectId);
+            const deal = await hubspotGetFromEndpoint(event[0].objectTypeId, event[0].objectId);
             if (deal.properties.usage_period && deal.properties.slut_projekt_period) {
                 console.log("Har en projektperiode!")
             } else {
                 console.log("Mangler projektperiode!")
             }
         }
-    } else if (event.subscriptionType === "object.propertyChange") {
+    } else if (event[0].subscriptionType === "object.propertyChange") {
         console.log("Det er change")
-        if (event.objectTypeId === "0-3") {
+        if (event[0].objectTypeId === "0-3") {
             console.log("Det er deal")
-            const deal = await hubspotGetFromEndpoint(event.objectTypeId, event.objectId);
+            const deal = await hubspotGetFromEndpoint(event[0].objectTypeId, event[0].objectId);
             if (deal.properties.usage_period && deal.properties.slut_projekt_period) {
                 console.log("Har en projektperiode!")
             } else {
