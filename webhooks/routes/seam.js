3a603727c420f8af7539235d52ef0c0ef3931a2f
Sylvester <sylvester@erbsledet.dk>
2025-10-30 17:36:54 +0100

    + seam

--- webhooks/routes/seam.js
+++ webhooks/routes/seam.js
@@ -9,10 +9,16 @@ const pool = require('../../db');
 const router = express.Router();
 router.use(express.json());
 
+import { Seam } from "seam";
+
+const seam = new Seam({ apiKey: process.env.SEAM_API });
+const acsSystemId = process.env.SEAM_ACS_SYSTEM_ID;
+const accessGroupId = process.env.SEAM_ACCESS_GROUP;
+
 router.post("/", async (req, res) => {
     const events = req.body;
-    console.log(events)
     res.status(200).send("OK");
+    console.log(events)
 });
 
 
