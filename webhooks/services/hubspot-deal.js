17140888470cc44de7b0bcd078f08000ae5fd1d7
Sylvester <sylvester@erbsledet.dk>
2026-02-23 15:25:32 +0100

    temp remove of pipeline check

--- webhooks/services/hubspot-deal.js
+++ webhooks/services/hubspot-deal.js
@@ -55,12 +55,12 @@ async function handleDealCreation(event) {
 
     const usagePeriod = deal.properties.usage_period;
     const endPeriod = deal.properties.slut_projekt_period;
-
+    /*
     if (deal.pipeline != "default") {
         logger.info('Ikke en udlejnings deal', { dealname: deal.properties.dealname });
         return true
     }
-
+    */
     if (!usagePeriod || !endPeriod) {
         logger.info('Deal mangler projektperiode', { dealname: deal.properties.dealname });
         return true;
